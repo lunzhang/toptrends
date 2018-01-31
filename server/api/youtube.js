95e4388a5bff37fffcbb24f3cab3ed69929edfa4
lunzhang <lun.zhang1993@gmail.com>
2018-01-31 16:09:14 -0500

    Update query api to single date

--- server/api/youtube.js
+++ server/api/youtube.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const YoutubeTrends = mongoose.model('YoutubeTrends');
-const { parseChartDate } = require('./utils');
 
 const youtubeTrends = (req, res) => {
-    let { beginDate, endDate } = parseChartDate(req);
+    let { date } = req.query;
 
-    YoutubeTrends.find({ date:  {$gte: beginDate, $lte: endDate}}, function(err, trends) {
+    if(!date) date = new Date().toLocaleDateString();
+
+    YoutubeTrends.find({ date }, (err, trends) => {
         if(err) res.sendStatus('400');
         res.json(JSON.stringify(trends));
     });
 };
 
 module.exports = {
-    youtubeTrends
+    youtubeTrends,
 };
