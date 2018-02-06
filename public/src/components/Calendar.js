a2c907a59d4138268af5c7e8a98a814481d739ae
lunzhang <lun.zhang1993@gmail.com>
2018-02-06 16:31:16 -0500

    Add chart content nav buttons

--- public/src/components/Calendar.js
+++ public/src/components/Calendar.js
@@ -26,7 +26,7 @@ export default class Calendar extends Component {
             this.props.container ?
             <InfiniteCalendar
                 className={ styles.calendarContainer }
-                height = { this.props.container.clientHeight - 147 }
+                height = { this.props.container.clientHeight - 144 }
                 min={ BEGIN_DATE } minDate={ BEGIN_DATE }
                 max={ this.today } maxDate={ this.today }
                 onSelect={ this.onSelect }
--- public/src/containers/Grid/YoutubeChart.js
+++ public/src/containers/Grid/YoutubeChart.js
@@ -16,11 +16,15 @@ const mapStateToProps = (state) => ({
 class YoutubeChart extends Component {
     constructor(props) {
         super(props);
+
         this.setContainer = this.setContainer.bind(this);
         this.onDateSelect = this.onDateSelect.bind(this);
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
         this.state = {
             contentContainer: null,
-            selectedDate: null,
+            selectedDate: today,
             currentView: 'Calendar',
         };
     }
@@ -36,16 +40,32 @@ class YoutubeChart extends Component {
         });
     }
 
+    setView(view) {
+        this.setState({
+            currentView: view
+        });
+    }
+
     render() {
         return (
             <ChartContainer  { ...this.props } { ...containerProps }>
                 <div className={ styles.chartContent } ref={ this.setContainer }>
                     {
                         this.state.currentView === 'Calendar' ?
-                        <Calendar container={ this.state.contentContainer } onDateSelect={ this.onDateSelect }/>
+                        <button type="button" className="btn btn-primary"
+                            onClick={() => this.setView('TrendView')}> Trend </button>
                         :
-                        <TrendView data={ this.props.data } date={ this.state.selectedDate }/>
+                        <button className={ styles.chartNavBtn }
+                            onClick={() => this.setView('Calendar')}> Back </button>
                     }
+                    <div>
+                        {
+                            this.state.currentView === 'Calendar' ?
+                            <Calendar container={ this.state.contentContainer } onDateSelect={ this.onDateSelect }/>
+                            :
+                            <TrendView data={ this.props.data } date={ this.state.selectedDate }/>
+                        }
+                    </div>
                 </div>
             </ChartContainer>
         );
